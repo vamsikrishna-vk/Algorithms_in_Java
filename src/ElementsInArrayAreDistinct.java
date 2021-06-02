9a4c2f0818ed82bd3c6313ccfd5744564b70ec5e
vamsi krishna <r18cs273@cit.reva.edu.in>
2021-06-02 15:31:15 +0530

    check if elements in array are unique

--- src/ElementsInArrayAreDistinct.java
+++ src/ElementsInArrayAreDistinct.java
@@ -4,7 +4,7 @@ public class ElementsInArrayAreDistinct {
         int[] arr={1,2,3,4,5,5}; // initialize and assign an integer array
         boolean result=true; // initialize and assign resut as true
         statement1:
-        for(int i=0;i<arr.length;i++){  // for loop for picking elements
+        for(int i=0;i<arr.length-1;i++){  // for loop for picking elements
 
             for(int j=0;j<arr.length;j++){ // for loop for comparing them with all other elements
                 if(arr[j]==arr[i]&& i!=j){ // check if the elements are equal
